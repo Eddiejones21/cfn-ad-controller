94270a9682cca227348595b8e50d33aa44906885
Eddie Jones <enzo@MacBook-Pro.local>
2023-12-28 23:01:18 -0500

    wip

--- install_tools.sh
+++ install_tools.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 INSTALL_BASE="/usr/local"
 
@@ -15,4 +15,49 @@ JQ_ARCHIVE="jq-1.7"
 JQ_URL="https://github.com/jqlang/jq/releases/download/$JQ_ARCHIVE/$JQ_ARCHIVE.tar.gz"
 
 
-# build and install autoco
+# build and install autoconf
+curl --location --remote-name --silent "$AUTOCONF_URL"
+tar --extract --file "$AUTOCONF_ARCHIVE.tar.gz"
+
+pushd "$AUTOCONF_ARCHIVE"
+./configure --prefix="$INSTALL_BASE"
+make
+sudo make install
+popd
+
+
+# build and install automake
+curl --location --remote-name --silent "$AUTOMAKE_URL"
+tar --extract --file "$AUTOMAKE_ARCHIVE.tar.gz"
+
+pushd "$AUTOMAKE_ARCHIVE"
+./configure --prefix="$INSTALL_BASE"
+make
+sudo make install
+popd
+
+
+# build and install libtool
+curl --location --remote-name --silent "$LIBTOOL_URL"
+tar --extract --file "$LIBTOOL_ARCHIVE.tar.gz"
+
+pushd "$LIBTOOL_ARCHIVE"
+./configure --prefix="$INSTALL_BASE" --disable-dependency-tracking --enable-ltdl-install
+make
+sudo make install
+popd
+
+
+# build and install jq
+curl --location --remote-name --silent "$JQ_URL"
+tar --extract --file "$JQ_ARCHIVE.tar.gz"
+pushd "$JQ_ARCHIVE"
+
+./configure --prefix="$INSTALL_BASE" --disable-maintainer-mode --with-oniguruma=builtin
+make
+sudo make install
+
+popd
+jq --version
+
+# done!
